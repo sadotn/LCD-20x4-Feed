222a5883b412ed865e5c3d34de21e604b8d538dc
CEMSguy <121373907+Danielerikrust@users.noreply.github.com>
2023-02-25 00:36:36 -0800

    Update mainLCD.py

--- mainLCD.py
+++ mainLCD.py
@@ -1,12 +1,13 @@
 import time
-from machine import I2C, Pin, UART
+from machine import I2C, Pin
 from LCD_Display import LcdApi, I2CLcd
 
 """LCD Section"""
 i2c = I2C(0, sda=Pin(20), scl=Pin(21), freq=400000) #GPIO 20 & 21 have no other function but I2C
 devices = i2c.scan()
 if devices != []:
-    lcd = I2CLcd(i2c, devices[0], 4, 20)
+    lcd = I2CLcd(i2c, devices[0], 4, 20) #For 4 row x 20 column LCDs
+    #lcd = I2CLcd(i2c, devices[0], 2, 16) #For 2 row x 16 column LCDs
 
 lcd.topdata("THIS REMAINS STATIC")
 time.sleep(1)
