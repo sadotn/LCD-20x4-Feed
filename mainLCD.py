391d2a6028e81902f0b6977ac22ace85bdba9ac6
CEMSguy <121373907+Danielerikrust@users.noreply.github.com>
2023-02-25 00:47:42 -0800

    Update mainLCD.py
    Improved the Pico Demo.

--- mainLCD.py
+++ mainLCD.py
@@ -9,7 +9,7 @@ if devices != []:
     lcd = I2CLcd(i2c, devices[0], 4, 20) #For 4 row x 20 column LCDs
     #lcd = I2CLcd(i2c, devices[0], 2, 16) #For 2 row x 16 column LCDs
 
-lcd.topdata("THIS REMAINS STATIC")
+lcd.topdata("THIS REMAINS STATIC") #Starts the top data line
 time.sleep(1)
 lcd.feed("^ These")
 time.sleep(1)
@@ -19,4 +19,13 @@ lcd.feed("^^^ Feed")
 time.sleep(1)
 lcd.feed("^^^^ Up")
 time.sleep(1)
-lcd.feed("as you add new ones")
+lcd.topdataline = False #Disables the top data line
+lcd.feed("^^^^^ As")
+time.sleep(1)
+lcd.feed("^^^^^^ You")
+time.sleep(1)
+lcd.feed("^^^^^^^ Add")
+time.sleep(1)
+lcd.topdata("  TOP LINE IS BACK  ") #Resumes the top data line
+time.sleep(1)
+lcd.feed("^^^^^^^^ New ones...")
